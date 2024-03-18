f46b235b087b776818f0a45cd57ffffa0a37f97b
pavanparamesh2001 <pavanparamesh4132@gmail.com>
2024-03-18 20:53:17 +0530

    restaurant-backend

---
+++ index.js
@@ -0,0 +1,24 @@
+//1 import json-server
+
+const jsonServer = require('json-server')
+
+//2 create a serever application using jsonServer
+const restServer = jsonServer.create()
+
+//3 setup path for db.json
+const router = jsonServer.router('db.json')
+
+//4 return middleware
+const middleware = jsonServer.defaults()
+
+//5 setup a port number
+const port = 3001
+
+//use in restServer
+restServer.use(middleware)
+restServer.use(router)
+
+// to run the server
+restServer.listen(port,()=>{
+    console.log("Restaurant server listening on port" + port);
+})
